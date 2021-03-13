d5fe62ba5690885efcc3ba3e681b4f83bfcaa3e2
ChauhanNikita398 <48960880+ChauhanNikita398@users.noreply.github.com>
2021-03-14 00:26:43 +0530

    Delete blog directory

--- blog/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Blog, Comment
-# Register your models here.
-
-admin.site.register(Blog)
-admin.site.register(Comment)
--- blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('',views.home, name='blog-home'),
-    path('browse',views.browse, name='browse'),
-    path('add',views.add, name='add'),
-]
